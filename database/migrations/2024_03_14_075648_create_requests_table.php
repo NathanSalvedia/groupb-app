155557bc9782a2c5719d8d322e559215a469d779
FLUFFY <157193026+NathanSalvedia@users.noreply.github.com>
2024-03-14 16:04:09 +0800

    initial commit

---
+++ database/migrations/2024_03_14_075648_create_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('request_form');
+            $table->string('typesof_requirements');
+            $table->string('files');
+            $table->string('pupose');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('requests');
+    }
+};
