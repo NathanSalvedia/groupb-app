5e0a4aca70cdaa2d9c0037e31e53d8ae4b69aeec
FLUFFY <157193026+NathanSalvedia@users.noreply.github.com>
2024-03-15 18:46:52 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RequestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+
+
+
+
+
+
+
